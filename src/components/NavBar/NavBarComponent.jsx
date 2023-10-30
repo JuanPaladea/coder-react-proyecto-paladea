6122ee7cab7470e8e6be5a3d6f8396fd85ed4a7e
JuanPaladea <juanpaladea5@gmail.com>
2023-10-30 19:09:20 -0300

    Cambios

--- src/components/NavBar/NavBarComponent.jsx
+++ src/components/NavBar/NavBarComponent.jsx
@@ -6,30 +6,27 @@ const NavBarComponent = () => {
             <nav className="navbar navbar-expand-lg pe-5">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#"><img className="logo" src="src\assets\img\logo.png" alt="logo libelulis"/></a>
-                    <button className="navbar-toggler me-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div>
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item mx-3 rounded">
-                                <a className="nav-link active" aria-current="page" href="#"> <i className="fa-sharp fa-solid fa-house "></i>
+                                <a className="nav-link active" aria-current="page" href="#">
                                 Inicio</a>
                             </li>
                             <li className="nav-item mx-3 rounded">
-                                <a className="nav-link" href="./pages/tienda.html"><i className="fa-sharp fa-solid fa-store"></i>
+                                <a className="nav-link" href="./pages/tienda.html">
                                 Tienda</a>
                             </li>
                             <li className="nav-item mx-3 rounded">
-                                <a className="nav-link" href="./pages/nosotros.html"><i className="fa-sharp fa-solid fa-address-card"></i>
+                                <a className="nav-link" href="./pages/nosotros.html">
                                 Nosotros</a>
                             </li>
                             <li className="nav-item mx-3 rounded">
-                                <a className="nav-link" href="./pages/talles.html"><i className="fa-sharp fa-solid fa-ruler-horizontal"></i>
-                                Guia de Talles</a>
+                                <a className="nav-link" href="./pages/contacto.html">
+                                Contacto</a>
                             </li>
                             <li className="nav-item mx-3 rounded">
-                                <a className="nav-link" href="./pages/contacto.html"><i className="fa-sharp fa-solid fa-message"></i>
-                                Contacto</a>
+                                <a className="nav-link" href="./pages/talles.html">
+                                Carrito</a>
                             </li>
                         </ul>
                     </div>
